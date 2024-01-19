54130697eb2082dc896957b814f544d16dfea910
Jiří "Wizard" Polášek <me@jiripolasek.com>
2024-01-19 10:37:51 +0100

    Update category name. (#9)

--- src/EditorBar/Options/GeneralPage.cs
+++ src/EditorBar/Options/GeneralPage.cs
@@ -11,16 +11,16 @@ namespace JPSoftworks.EditorBar.Options;
 
 public class GeneralPage : BaseOptionModel<GeneralPage>
 {
-    [Category("Apperance")]
+    [Category("Appearance")]
     [DisplayName("Bar position")]
-    [Description("Position of the editor bar (change will be applied to a newly opened documents)")]
+    [Description("Position of the editor bar (change will be applied to a newly opened documents).")]
     [DefaultValue(Options.BarPosition.Top)]
     [TypeConverter(typeof(EnumConverter))]
     public BarPosition BarPosition { get; set; } = BarPosition.Top;
 
-    [Category("Apperance")]
+    [Category("Appearance")]
     [DisplayName("Show relative path")]
-    [Description("Show path relative to the solution root")]
+    [Description("Show path relative to the solution root.")]
     [DefaultValue(true)]
     public bool ShowPathRelativeToSolutionRoot { get; set; } = true;
 
