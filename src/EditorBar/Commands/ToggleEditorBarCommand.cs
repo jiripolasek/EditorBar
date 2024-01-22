4a8ee80eb12e7af17f2611a4bcc38ee1f8b7acfc
Jiří "Wizard" Polášek <me@jiripolasek.com>
2024-01-22 22:03:19 +0100

    Adds a command to toggle Editor Bar on and off, adds that command to View menu and to Text Editor standard toolbar.

---
+++ src/EditorBar/Commands/ToggleEditorBarCommand.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------------------------------------------
+//
+// Copyright (c) Jiří Polášek. All rights reserved.
+//
+// ------------------------------------------------------------
+
+using Community.VisualStudio.Toolkit;
+using JPSoftworks.EditorBar.Options;
+using Microsoft.VisualStudio.Shell;
+
+namespace JPSoftworks.EditorBar.Commands;
+
+[Command(PackageGuids.guidEditorBarPackageCmdSetString, PackageIds.ToggleEditorBarCommand)]
+internal sealed class ToggleEditorBarCommand : BaseCommand<ToggleEditorBarCommand>
+{
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        GeneralPage.Instance.Enabled = !GeneralPage.Instance.Enabled;
+        await GeneralPage.Instance.SaveAsync();
+    }
+}
