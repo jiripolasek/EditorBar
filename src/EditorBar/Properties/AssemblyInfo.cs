bbe2424abbcde45d945a5c62f1c432a6fb4f7d9b
Jiří "Wizard" Polášek <me@jiripolasek.com>
2024-01-25 20:38:56 +0100

    Synchronize assembly and file versions with VSIX version.

--- src/EditorBar/Properties/AssemblyInfo.cs
+++ src/EditorBar/Properties/AssemblyInfo.cs
@@ -6,6 +6,7 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
+using JPSoftworks.EditorBar;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion(Vsix.Version + ".*")]
+[assembly: AssemblyFileVersion(Vsix.Version)]
