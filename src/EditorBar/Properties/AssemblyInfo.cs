b28e2248d5715320edd12d12951986e5f75c095c
Jiří "Wizard" Polášek <me@jiripolasek.com>
2024-01-25 20:38:56 +0100

    Synchronize assembly and file versions with VSIX version.

--- src/EditorBar/Properties/AssemblyInfo.cs
+++ src/EditorBar/Properties/AssemblyInfo.cs
@@ -6,15 +6,16 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
+using JPSoftworks.EditorBar;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Editor Bar")]
-[assembly: AssemblyDescription("Visual Studio Extension")]
+[assembly: AssemblyTitle(Vsix.Name)]
+[assembly: AssemblyDescription(Vsix.Description)]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jiří Polášek")]
-[assembly: AssemblyProduct("Editor Bar")]
+[assembly: AssemblyProduct(Vsix.Name)]
 [assembly: AssemblyCopyright("(c) 2024 Jiří Polášek")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,5 +35,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion(Vsix.Version + ".*")]
+[assembly: AssemblyFileVersion(Vsix.Version)]
