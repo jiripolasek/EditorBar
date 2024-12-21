4961bbb99c94d3c04be4beed159388fd7ac5ce2e
Jiří "Wizard" Polášek <me@jiripolasek.com>
2024-12-21 19:50:50 +0100

    Updates version to be 3 parts (1.1 -> 1.1.0)

--- src/EditorBar/source.extension.cs
+++ src/EditorBar/source.extension.cs
@@ -11,7 +11,7 @@ namespace JPSoftworks.EditorBar
         public const string Name = "Editor Bar";
         public const string Description = @"Shows location of the current file and highlights important parts like parent project or folder";
         public const string Language = "en-US";
-        public const string Version = "1.1";
+        public const string Version = "1.1.0";
         public const string Author = "Jiří Polášek";
         public const string Tags = "editor bar,path,file,breadcrumbs,location,project,directory,folder";
     }
